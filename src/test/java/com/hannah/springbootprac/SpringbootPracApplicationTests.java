790ffed263b002e4a49e24e225867178804759b2
Hannah0su <luvmyselfh@gmail.com>
2023-03-23 23:39:44 +0900

    :tada: 프로젝트 생성

---
+++ src/test/java/com/hannah/springbootprac/SpringbootPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hannah.springbootprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
