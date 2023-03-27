954b52ab27f437d125b91a49354a154f95660e2e
Hannah0su <luvmyselfh@gmail.com>
2023-03-27 17:26:35 +0900

    :heavy_plus_sign: JPA 관련 설정 작성

--- src/main/java/com/hannah/springbootprac/SpringbootPracApplication.java
+++ src/main/java/com/hannah/springbootprac/SpringbootPracApplication.java
@@ -2,12 +2,14 @@ package com.hannah.springbootprac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringbootPracApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootPracApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPracApplication.class, args);
+    }
 
 }
