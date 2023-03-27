c8d69d16e2a2d474a12c22944bd6087e8e0b6377
Hannah0su <luvmyselfh@gmail.com>
2023-03-27 17:36:02 +0900

    :white_check_mark: Post 작성,수정 테스트 작성

---
+++ src/test/java/com/hannah/springbootprac/post/controller/PostApiControllerTest.java
@@ -0,0 +1,103 @@
+package com.hannah.springbootprac.post.controller;
+
+import com.hannah.springbootprac.post.dto.PostCreateRequestDto;
+import com.hannah.springbootprac.post.dto.PostUpdateRequestDto;
+import com.hannah.springbootprac.post.entity.Post;
+import com.hannah.springbootprac.post.repository.PostRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PostApiControllerTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @AfterEach
+    public void tearDown() throws Exception{
+        postRepository.deleteAll();
+    }
+
+    @Test
+    public void Post_등록() throws Exception{
+        //given
+        String title = "title";
+        String content = "content";
+        PostCreateRequestDto requestDto = PostCreateRequestDto.builder()
+                .title(title)
+                .content(content)
+                .author("hannah0su")
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts";
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate
+                .postForEntity(url, requestDto, Long.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Post> all = postRepository.findAll();
+
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+
+    }
+
+    @Test
+    public void Post_수정() throws Exception {
+        //given
+        Post savedPost = postRepository.save(Post.builder()
+                .title("title")
+                .content("content")
+                .author("hannah0su")
+                .build());
+
+        Long updatedId = savedPost.getId();
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        PostUpdateRequestDto requestDto = PostUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updatedId;
+
+        HttpEntity<PostUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+        List<Post> all = postRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
+    }
+
+
+}
