dcba36f518d8e43791875ef199491c8b666b0e26
Hannah0su <luvmyselfh@gmail.com>
2023-03-27 17:33:26 +0900

    :white_check_mark: Post 작성,수정,조회 기능 구현

---
+++ src/main/java/com/hannah/springbootprac/post/dto/PostResponseDto.java
@@ -0,0 +1,20 @@
+package com.hannah.springbootprac.post.dto;
+
+import com.hannah.springbootprac.post.entity.Post;
+import lombok.Getter;
+
+@Getter
+public class PostResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostResponseDto(Post entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
